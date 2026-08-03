8361bd402bfac9b2ab7027a4cea98ef416dc74ef
mingfei80 <mingfei80@yahoo.com>
2026-08-03 22:29:27 +0100

    Initial commit: Professional SOLID framework structure using VS 2026 .slnx

---
+++ UniversalMediaSorter/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UniversalMediaSorter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
